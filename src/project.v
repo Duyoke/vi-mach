68ce5b0743c60ca54d9e6b84f956a36d4dabd048
Duyoke <trduy142007@gmail.com>
2025-07-02 15:40:52 +0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -28,7 +28,7 @@ module tt_um_asiclab_example (
         if(reset) begin
             uo_out <= 0;
         end else begin
-            uo[3:0] <= ui_in[7:4] + ui_in[3:0];
+            uo_out[3:0] <= ui_in[7:4] + ui_in[3:0];
             uo_out[7:4] <= 0;
         end
     end
